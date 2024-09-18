146746874e69423567bf5f04f2047c655ed27b48
rworsham <robertworsham01@gmail.com>
2024-09-17 20:58:49 -0400

    Connected project to hosted postgresql db, migrated models to said database

--- my_cooking_helper/my_cooking_helper/settings.py
+++ my_cooking_helper/my_cooking_helper/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv, dotenv_values
+import dj_database_url
 
 load_dotenv()
 
@@ -86,14 +87,10 @@ WSGI_APPLICATION = 'my_cooking_helper.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DB_HOST'),
+        conn_max_age=45
+    )
 }
 
 
