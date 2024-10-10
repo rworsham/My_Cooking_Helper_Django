577771eeb6c73a1f7bbfd7cd7554b543c8ad2fe9
rworsham <robertworsham01@gmail.com>
2024-10-09 21:11:52 -0400

    Adjusted background for featured recipes to conform with style, added favorites button to dashboard, expanding recipe detail to include an option for having favorites added js to support this behavior. need to connect view and test form submission

---
+++ my_cooking_helper/static/js/favorite.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const favoriteIcon = document.querySelector('.favorite span');
+    const favoriteInput = document.getElementById('favorite');
+    const form = document.getElementById('favoriteForm');
+
+    favoriteIcon.addEventListener('click', () => {
+        if (favoriteInput.value === 'favorite') {
+            favoriteInput.value = 'un_favorite';
+            favoriteIcon.classList.remove('bi-heart-fill');
+            favoriteIcon.classList.add('bi-heart');
+        } else {
+            favoriteInput.value = 'favorite';
+            favoriteIcon.classList.remove('bi-heart');
+            favoriteIcon.classList.add('bi-heart-fill');
+        }
+
+        form.submit();
+    });
+});
